2e32e53b46c90474f209b225f70a6ccf35240942
GWHM0603 <uhmin0012@gmail.com>
2026-04-17 13:49:15 +0900

    함수 + 전역지역변수 확인

--- DAY07/ex_funtion_01.py
+++ DAY07/ex_funtion_01.py
@@ -2,10 +2,12 @@
 # 함수: def():
 # ===== ===== ===== ===== =====
 
-
+a = 3
 def add1(a, b):
     return a + b
+    print(a)
 print(add1(2, 3))
+print(a)
 
 
 def add2(a, b):
@@ -13,7 +15,7 @@ def add2(a, b):
 add2(2, 3)
 
 
-def add3():
+def add3(): # 매개변수 없는 함수에 파라미터 입력하면 에러
     print(3 + 2)
 add3()
 
